cb11efaf746bcba70102449d6a73c306410e5bbd
Gabriela <gabriela.priaro@gmail.com>
2024-10-26 12:21:40 +0100

    Implementaçao continua para definir ordem crescente ou decrescente entre dois numeros, até que os numeros sejam iguais.

--- CrescenteOuDecrescente/Program.cs
+++ CrescenteOuDecrescente/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace CrescenteOuDecrescente
+﻿//Problema "crescente" (adaptado de URI 1113)
+//Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. Escreva para cada X e Y uma
+//mensagem que indique se estes valores foram digitados em ordem crescente ou decrescente. O
+//programa deve finalizar quando forem digitados dois valores iguais. 
+
+namespace CrescenteOuDecrescente
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x, y;
+            
+            Console.WriteLine("Digite dois numeros:");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("CRESCENTE!");
+                }
+                else
+                {
+                    Console.WriteLine("DECRESCENTE!");
+                }
+
+                Console.WriteLine("Digite outros dois numeros:");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
